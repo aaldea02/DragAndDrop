7716ffa19358bdc6485d4a30bf00debd3c65aa41
aaldea02 <aaldea@perfectserve.com>
2022-08-04 14:48:56 -0400

    Latest

--- public/dropdown.js
+++ public/dropdown.js
@@ -1,5 +1,56 @@
 import { draggableList } from "./dragdrop.js";
 document.addEventListener("DOMContentLoaded", () => {
+  let choice = -1;
+
+  const div1 = document.getElementById("div1");
+  const autocomplete = document.createElement("span"); // make an array later on
+  const autocomplete_result = document.createElement("span");
+  const hiddenParagraph = document.createElement("span");
+  const autocomplete_input = document.createElement("span");
+
+  const getCaretCoordinates = () => {
+    let x = 0,
+      y = 0;
+    const isSupported = typeof window.getSelection !== "undefined";
+    if (isSupported) {
+      const selection = window.getSelection();
+      // Check if there is a selection (i.e. cursor in place)
+      if (selection.rangeCount !== 0) {
+        // Clone the range
+        const range = selection.getRangeAt(0).cloneRange();
+        // Collapse the range to the start, so there are not multiple chars selected
+        range.collapse(true);
+        // getCientRects returns all the positioning information we need
+        const rect = range.getClientRects()[0];
+        if (rect) {
+          x = rect.left; // since the caret is only 1px wide, left == right
+          y = rect.top; // top edge of the caret
+        }
+      }
+    }
+    return { x, y };
+  };
+
+  const moveAutocompleteResult = (event, contenteditable) => {
+    const tooltip = document.getElementById("autocomplete_result");
+    if (contenteditable.contains(event.target)) {
+      const { x, y } = getCaretCoordinates();
+      tooltip.setAttribute("aria-hidden", "false");
+      tooltip.setAttribute(
+        "style",
+        `display: inline-block; left: ${x}px; top: ${y - 200}px` // unfortunately this is a magic number
+      );
+    } else {
+      tooltip.setAttribute("aria-hidden", "true");
+      tooltip.setAttribute("style", "display: none;");
+    }
+  };
+
+  const clearPopup = () => {
+    autocomplete_result.innerHTML = "";
+    autocomplete_result.style.display = "none";
+  };
+
   const placeIbeamAfterNode = (node) => {
     if (typeof window.getSelection != "undefined") {
       const range = document.createRange();
@@ -11,19 +62,11 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const div1 = document.getElementById("div1");
-  const autocomplete = document.createElement("span"); // make an array later on
-  const autocomplete_result = document.createElement("span");
-
-  const clearPopup = () => {
-    autocomplete_result.innerHTML = "";
-    autocomplete_result.style.display = "none";
-  };
-
   const completeInput = (node) => {
     setTimeout(function () {
       div1.focus();
     }, 0);
+
     placeIbeamAfterNode(autocomplete);
     div1.setAttribute("contentEditable", true);
     autocomplete.setAttribute("contentEditable", false);
@@ -31,9 +74,31 @@ document.addEventListener("DOMContentLoaded", () => {
     clearPopup();
   };
 
-  const popup = (node) => {
-    const regExp = new RegExp("^" + autocomplete.innerText, "i");
+  const selectChoice = (node, choices) => {
+    if (choice >= 0) {
 
+      const child = autocomplete_result.children[choice];
+      child.style.backgroundColor = "pink";
+     // node.innerText = child.innerText;
+    }
+  };
+  const clickChoice = (node, choices) => {
+    console.log("cool");
+    
+    node.innerText = choices.innerText;
+    autocomplete_result.innerHTML = "";
+    autocomplete_result.style.display = "none";
+    placeIbeamAfterNode(autocomplete);
+    div1.setAttribute("contentEditable", true);
+    autocomplete.setAttribute("contentEditable", false);
+    completeInput();
+    clearPopup();
+  };
+
+  const popup = (node) => {
+    const innertext = autocomplete.innerText;
+    const regExp = new RegExp("^" + node.innerText, "i");
+    console.log(innertext);
     const fragment = document.createDocumentFragment();
 
     let flag = false;
@@ -41,14 +106,12 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let x = 0; x < draggableList.length; x++) {
       if (regExp.test(draggableList[x].content)) {
         flag = true;
-
+        
         const choices = document.createElement("p");
         choices.innerText = draggableList[x].content;
         choices.setAttribute("class", "choices");
-        choices.setAttribute(
-          "onclick",
-          "autocomplete.innerText=this.innerText;autocomplete_result.innerHTML='';autocomplete_result.style.display='none'; completeInput()"
-        );
+        choices.addEventListener("click", () => clickChoice(node, choices));
+        // make into an event listener
 
         fragment.appendChild(choices);
       }
@@ -60,12 +123,6 @@ document.addEventListener("DOMContentLoaded", () => {
     autocomplete_result.innerHTML = "";
     autocomplete_result.style.display = "block";
     autocomplete_result.appendChild(fragment);
-  };
-
-  const selectChoice = (node) => {
-    const child = autocomplete_result.children[0];
-    child.style.backgroundColor = "pink";
-    //alert("wow");
   };
 
   const insertNode = (node) => {
@@ -114,8 +171,8 @@ document.addEventListener("DOMContentLoaded", () => {
       autocomplete.focus();
     }, 0);
 
-    autocomplete.addEventListener("keyup", popup); // passing in the node argument breaks it
-    autocomplete.addEventListener("change", popup);
+    autocomplete.addEventListener("keyup", () => popup(autocomplete)); // passing in the node argument breaks it
+    autocomplete.addEventListener("change", () => popup(autocomplete));
   };
 
   const deleteInput = (node) => {
@@ -123,7 +180,8 @@ document.addEventListener("DOMContentLoaded", () => {
       autocomplete.blur();
     }, 0);
 
-    div1.removeChild(autocomplete);
+    autocomplete.remove(); // might not work on IE
+
     div1.removeChild(autocomplete_result);
     div1.setAttribute("contentEditable", true);
 
@@ -138,7 +196,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const checkKeyPressed = (evt) => {
     if (evt.key === "@") {
+      
       createInput();
+      moveAutocompleteResult(evt, div1);
     } else if (
       evt.key === "Backspace" &&
       div1.contains(document.getElementById("autocomplete")) &&
@@ -151,9 +211,16 @@ document.addEventListener("DOMContentLoaded", () => {
     ) {
       completeInput();
     } else if (
-      evt.key === "ArrowDown" && 
+      evt.key === "ArrowDown" &&
       div1.contains(document.getElementById("autocomplete"))
     ) {
+      choice++;
+      selectChoice();
+    } else if (
+      evt.key === "ArrowUp" &&
+      div1.contains(document.getElementById("autocomplete"))
+    ) {
+      choice--;
       selectChoice();
     }
   };
